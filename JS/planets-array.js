fb0b2353281a0511d27259911b272d55d62b085d
patrick <patricklarkin995@gmail.com>
2020-08-20 16:14:58 -0500

    Planets-JS finished

---
+++ JS/planets-array.js
@@ -0,0 +1,61 @@
+(function () {
+	"use strict";
+
+	var planets = [
+		'Mercury',
+		'Venus',
+		'Earth',
+		'Mars',
+		'Jupiter',
+		'Saturn',
+		'Uranus',
+		'Neptune'
+	];
+
+	// /** TODO: Read each console log below, and write some javascript code to perform the step that it describes */
+	console.log('Adding "The Sun" to the beginning of the planets array.');
+	planets.unshift("Sun");
+	console.log(planets);
+
+	console.log('Adding "Pluto" to the end of the planets array.');
+	planets.push("Pluto");
+	console.log(planets);
+
+	console.log('Removing "The Sun" from the beginning of the planets array.');
+	planets.shift();
+	console.log(planets);
+
+	console.log('Removing "Pluto" from the end of the planets array.');
+	planets.pop();
+	console.log(planets);
+
+	console.log('Finding and logging the index of "Earth" in the planets array.');
+	console.log(planets.indexOf("Earth"));
+
+	console.log("Reversing the order of the planets array.");
+	planets.reverse();
+	console.log(planets);
+
+	console.log("Sorting the planets array.");
+	planets.sort();
+	console.log(planets);
+
+	/////////////////////////////////////////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+	var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+	var planetsArray;
+
+	/** TODO: Convert planetsString to an array, and save it in a variable named planetsArray. console.log planetsArray to check your work */
+	planetsArray = planetsString.split("|");
+	console.log(planetsArray);
+
+	/** TODO: Create a string with <br> tags between each planet. console.log() your results. Why might this be useful?
+	 * BONUS:
+	 * Create another string that would display your planets in an undordered
+	 * list. You will need an opening AND closing <ul> tags around the entire
+	 * string, and <li> tags around each planet.
+	 */
+	console.log(planetsArray.join("<br>"));
+	console.log('<ul>\n   <li>' + planetsArray.join('</li>\n   <li>') + '</li>\n</ul>')
+
+})();
