9fa2e44b73b3be995bc4ba0c05869e5af29d5fde
patrick <patricklarkin995@gmail.com>
2020-08-25 23:07:03 -0500

    Bom-lec not completed

---
+++ JS/fiz-buzz.js
@@ -0,0 +1,56 @@
+// for(let i = 1; i < 100; i++) {
+// 	if(i % 3 === 0 && i % 5 === 0) {
+// 		console.log(i + ". buzzzfizzz");
+// 	} else if (i % 5 === 0) {
+// 		console.log(i + ". fizzz");
+// 	} else if (i % 3 === 0) {
+// 		console.log(i + ". buzzz");
+// 	} else {
+// 		console.log(i + ". is a number")
+// 	}
+// }
+
+'use strict';
+
+// this code will produce a console log every second
+// when count >= max, the interval is cancelled, and the logging will stop
+
+// var count = 0;
+// var max = 5;
+// var interval = 2000; // interval time in milliseconds
+//
+// var intervalId = setInterval(function () {
+// 	if (count >= max) {
+// 		clearInterval(intervalId);
+// 		console.log('All done');
+// 	} else {
+// 		count++;
+// 		alert('Repeating this line ' + count);
+// 	}
+// }, interval);
+
+
+// var count = 0;
+// var max = 5;
+// var delay = 5000; // interval time in milliseconds
+//
+// var timeoutId = setInterval(function () {
+// 	alert('3 second rule the food is still safe!');
+// 	//redirects to web page
+// 	location = 'https://www.google.com/';
+// }, delay);
+
+	//to cancel the timeout, you can call
+	//clearTimeout(timeout)
+	//prior to delay expiring
+
+
+// 	if (count >= max) {
+// 		clearInterval(intervalId);
+// 		console.log('All done');
+// 	} else {
+// 		count++;
+// 		alert('Repeating this line ' + count);
+// 	}
+// }, interval);
+
