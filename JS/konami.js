b505293fc3eafc5e86de273f17b933d845c18526
patrick <patricklarkin995@gmail.com>
2020-09-08 10:36:03 -0500

    progress on national parks faq project

--- JS/konami.js
+++ JS/konami.js
@@ -1,8 +1,9 @@
 (function () {
 	"use strict";
-	$().ready(function () {
+	$(document).ready(function () {
 		$(".winAlert").hide();
 		$(".loseAlert").hide();
+
 
 		$(document).keyup(function(event){
 			console.log(event.keyCode);
@@ -10,20 +11,31 @@
 
 		function konamiDecoder() {
 			var userInput = ''
-			var code = "3838404037393739666513"
+			var konamiCode = "3838404037393739666513"
 			var winMusic = new Audio('../Media/8-bit.mp3');
 			var loseMusic = new Audio('../Media/Doot.mp3');
 			$(document).keydown(function () {
 				userInput += ("" + event.keyCode);
-				if (userInput === code) {
+				if (userInput === konamiCode) {
+					$(".default").hide();
 					winMusic.play();
 					$(".winAlert").show();
-				} else if (userInput.length > 22){
-					loseMusic.play()
-					$(".loseAlert").show();
+				} else if (userInput !== konamiCode){
+					// $(".default").hide();
+					// loseMusic.play()
+					// $(".loseAlert").show();
+					$(".default").hide();
+					winMusic.play();
+					$(".winAlert").show();
 				}
 			});
 		}
 		konamiDecoder();
 	});
 })();
+
+
+
+//Todo: Start Build interactive remote
+//Todo: Start Real code comparison loop
+//Todo: Catch up
