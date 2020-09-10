6e80dee1da81a35c150e3825a5c7384e2b3cf9b7
patrick <patricklarkin995@gmail.com>
2020-09-10 08:57:09 -0500

    taking a break from blog & json exercise

--- JS/json-blog.js
+++ JS/json-blog.js
@@ -1,23 +1,29 @@
-(function () { // IFEE
-	"use strict"
+(function () {
 	$().ready(function () {
-		$.ajax("../JSON/blog.json").done(function (data) {
-			let titleInnerHTML = '';
-			let contentInnerHTMl = '';
-			let tagsInnerHTML = '';
-			let dateInnerHTML = '';
-			data.forEach(function (item) {
-				titleInnerHTML += item['title']
-				$('#title').html(titleInnerHTML);
-
-				contentInnerHTMl += item['content']
-				$('#content').html(contentInnerHTMl);
-
-				tagsInnerHTML += item['categories']
-				$('#categories').html(tagsInnerHTML);
-
-				dateInnerHTML += item['date']
-				$('#date').html(dateInnerHTML);
-			});
+		"use strict"
+		$.get("../JSON/blog.json").done(function (data, status) {
+			alert("AJAX call completed successfully!");
+			console.log("Request status: " + status);
+			console.log("Data returned from server:");
+			console.log(data);
 		});
-	})();
+	});
+})();
+// 				function createCard() {
+// 			data.forEach(function (item) {
+// 					var html = '<div className="col mb-4">';
+// 					html += '<div className="card">';
+// 					html += '<h5 className="card-title __identify__">' + item['title'] + '</h3>';
+// 					html += '<p className="card-text" id="content">' + item['content'] + '</p>';
+// 					html += '<div className="card-footer">';
+// 					html += '<button type="button" className="btn btn-outline-primary" id="catagories">' + +'</button>';
+// 					html += '<p className="card-text" id="date"><small className="text-muted"></small>' + item['date'] + '</p>';
+// 					html += '</div>';
+// 					html += '</div>';
+// 					html += '</div>';
+// 					$('.cardTemplate').html(html);
+// 				}
+// 			});
+// 		});
+// 	});
+// })();
